3b27d72a2b00502aec2bf730aaec4c16b33cba67
abhi55555 <33001714+abhi55555@users.noreply.github.com>
2019-05-02 23:36:28 +0530

    Practice

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -3,10 +3,8 @@ def removeDuplicates(A):
     for a in A:
         if a in table:
             table[a] += 1
-        elif a not in table:
-            table[a] = 1
         else:
-            table[a] += 0
+            table[a] = 1
 
     newtable = []
     [newtable.append(t) for t in table.keys()]
@@ -28,6 +26,18 @@ def removeDuplicates3(A):
     print(list(dict.fromkeys(A)))
 
 
-A = [4, 2, 2, 3, 22, 3, 3, 14, 5, 17, 9]
+def removeDuplicates_sorted(A):         # requires sorted array
 
+    j = 0
+    for i in range(1, len(A)):
+        if A[j] != A[i]:
+            j += 1
+            A[j] = A[i]
+
+    print(A[:j + 1])
+
+
+A = [4, 2, 2, 3, 22, 3, 3, 14, 5, 17, 9]
+A_sorted = [0, 2, 2, 2, 3, 3, 4, 5, 7, 9]
 removeDuplicates3(A)
+removeDuplicates_sorted(A_sorted)
